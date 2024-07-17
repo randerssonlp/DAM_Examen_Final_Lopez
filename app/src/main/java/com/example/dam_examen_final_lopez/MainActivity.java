5b837dfd367755a238ea34d40c1522b3fef3886e
RAUL ANDERSSON LOPEZ PURIS <20a3110155@untels.edu.pe>
2024-07-17 10:01:43 -0500

    Commit 02: Notas en Columnas

--- app/src/main/java/com/example/dam_examen_final_lopez/MainActivity.java
+++ app/src/main/java/com/example/dam_examen_final_lopez/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.dam_examen_final_lopez;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin = findViewById(R.id.buttonLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Iniciar la actividad NotasActivity
+                startActivity(new Intent(MainActivity.this, NotasActivity.class));
+
+                // Mostrar un mensaje de sesión iniciada
+                Toast.makeText(MainActivity.this, "Sesión iniciada", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
